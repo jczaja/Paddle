ea02f60a0ddb4e07d6dbf20e0bfd81105c74bd0e
Jacek Czaja <jacek.czaja@intel.com>
2019-11-04 16:52:11 +0100

    - MUL compilation fix

--- paddle/fluid/operators/mkldnn/mul_mkldnn_op.cc
+++ paddle/fluid/operators/mkldnn/mul_mkldnn_op.cc
@@ -413,7 +413,7 @@ class MulMKLDNNKernel : public framework::OpKernel<XT> {
     if (out_dims.size() != 2) {
       out->Resize(out_dims);
       // perhaps it was flattened
-      memory::data_type out_type = ToMKLDNNDataType(out->type());
+      memory::data_type out_type = paddle::framework::ToMKLDNNDataType(out->type());
       out->set_mkldnn_mem_desc({out_dims, out_type, platform::MKLDNNFormatForSize(out_dims.size(), MKLDNNMemoryFormat::nchw)});
     } else {
       out->set_mkldnn_mem_desc(mul->dst_desc());
