5f6fbcf1f2293b95209ca51bec2c02e1eba28007
Jacek Czaja <jacek.czaja@intel.com>
2020-05-21 17:11:56 +0200

    - fix

--- paddle/fluid/framework/ir/mkldnn/mkldnn_conv_bn_fuse_pass_tester.cc
+++ paddle/fluid/framework/ir/mkldnn/mkldnn_conv_bn_fuse_pass_tester.cc
@@ -139,7 +139,7 @@ class MKLDNNConvBatchNormPassTest {
 
     graph->SetNotOwned(kParamScopeAttr, &scope);
 
-    GenScopeTreeDebugInfo(scope);
+    GenScopeTreeDebugInfo(&scope);
 
     auto *my_var = scope.FindVar("a");
 
