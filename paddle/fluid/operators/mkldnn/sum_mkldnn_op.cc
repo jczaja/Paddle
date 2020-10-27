997f1daf269fc452b87d34d2e4b7aef1b2fccc47
Jacek Czaja <jacek.czaja@intel.com>
2020-10-27 16:45:31 +0100

    - fix

--- paddle/fluid/operators/mkldnn/sum_mkldnn_op.cc
+++ paddle/fluid/operators/mkldnn/sum_mkldnn_op.cc
@@ -179,8 +179,8 @@ class SumMKLDNNOpKernel : public paddle::framework::OpKernel<T> {
           framework::vectorize(output->dims()), ctx.OutputName("Out") + "-I");
 
       platform::ReorderMKLDNNHandler reorder_handler(
-           dst_mem->desc().dims(), output->type(), dst_mem->desc().type(),
-           dev_ctx, dev_ctx.GetEngine());
+           framework::vectorize<int64_t>(output->dims()), output->type(), 
+           dst_mem->desc().type(), dev_ctx, dev_ctx.GetEngine());
 
       auto& in_out = in_vars[0]->Get<framework::LoDTensor>();
       auto target_mem = handler.AcquireDstMemory(output, in_out.format());
