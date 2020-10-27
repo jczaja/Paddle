86e742ed223988e3f1cda3dcd0e316fd18a04163
Jacek Czaja <jacek.czaja@intel.com>
2020-10-27 17:46:53 +0100

    - fix

--- paddle/fluid/operators/mkldnn/sum_mkldnn_op.cc
+++ paddle/fluid/operators/mkldnn/sum_mkldnn_op.cc
@@ -184,7 +184,7 @@ class SumMKLDNNOpKernel : public paddle::framework::OpKernel<T> {
            output_tz, output->type(), framework::ToMKLDNNDataType(in_out.type()),
            dev_ctx, dev_ctx.GetEngine(), reorder_key);
 
-      auto target_mem = reorder_handler.AcquireDstMemory(&output, in_out.format(),
+      auto target_mem = reorder_handler.AcquireDstMemory(output, in_out.format(),
            ctx.GetPlace());
 
       auto reorder_p = reorder_handler.AcquireReorder(target_mem, dst_mem);
