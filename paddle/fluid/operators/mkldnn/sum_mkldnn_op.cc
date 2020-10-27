e35757b8f96f54b389450b49c44e5b664623ac5b
Jacek Czaja <jacek.czaja@intel.com>
2020-10-27 16:02:35 +0100

    - compilation fix

--- paddle/fluid/operators/mkldnn/sum_mkldnn_op.cc
+++ paddle/fluid/operators/mkldnn/sum_mkldnn_op.cc
@@ -174,11 +174,11 @@ class SumMKLDNNOpKernel : public paddle::framework::OpKernel<T> {
     // so from oneDNN dst memory we reorder data into input
     if (in_place) {
       const std::string reorder_key = platform::CreateKey(
-          framework::vectorize(output->dims()), ctx.OutputName("Out") + "-I")
+          framework::vectorize(output->dims()), ctx.OutputName("Out") + "-I");
 
-          platform::ReorderMKLDNNHandler reorder_handler(
-              dst_mem->desc().dims(), output->type(), dst_mem->desc().type(),
-              *dev_ctx, dev_ctx.GetEngine());
+      platform::ReorderMKLDNNHandler reorder_handler(
+           dst_mem->desc().dims(), output->type(), dst_mem->desc().type(),
+           *dev_ctx, dev_ctx.GetEngine());
 
       auto& in_out = in_vars[0]->Get<framework::LoDTensor>();
       auto target_mem = handler.AcquireDstMemory(output, in_out->format());
