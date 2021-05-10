a5dbbdfd4484af7edbe2566a0407086329bc4394
Jacek Czaja <jacek.czaja@intel.com>
2021-05-10 10:03:10 +0200

    - Huge Pages without sequential advice

--- paddle/fluid/memory/detail/system_allocator.cc
+++ paddle/fluid/memory/detail/system_allocator.cc
@@ -70,7 +70,7 @@ void* AlignedMalloc(size_t size) {
 #ifdef __linux__
   // Advise may fail due to platform capabilities
   // so we will ignore returned error
-  error = madvise(p, size, MADV_HUGEPAGE | MADV_SEQUENTIAL);
+  error = madvise(p, size, MADV_HUGEPAGE);
   if (error != 0) {
     VLOG(4) << "Memory Advice (madvice) ignored for size: " << size
             << " due to error: " << strerror(errno);
